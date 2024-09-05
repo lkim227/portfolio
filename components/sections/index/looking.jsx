bbf2fdfee8389ced5dd5b04501c6c228119cbf4f
lkim227 <lucas.kim98227@gmail.com>
2024-09-05 02:59:33 +0200

    looking.jsx

--- components/sections/index/looking.jsx
+++ components/sections/index/looking.jsx
@@ -29,8 +29,6 @@ export default function Looking() {
 				<h2 className={looking.jsonSub}><span className={looking.highlight2}>User Experience Designer</span></h2>
 				<h2 className={looking.json}>&#125;</h2>
 				<h4>I am particularily interested in product facing postions where I can help make an organization wide impact.</h4>
-				{/* <h2>Senior <span className={looking.highlight}>User Experience</span> Designer</h2> */}
-				{/* <p className="subtitle">with a focus on Product Design.</p> */}
 			</Container>
 		</Section>
 	)
